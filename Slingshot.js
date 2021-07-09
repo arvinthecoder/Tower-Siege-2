cd475f5fefa3899f301a3258171548284d296fe2
arvinthecoder <63166353+arvinthecoder@users.noreply.github.com>
2021-07-08 17:54:13 -0700

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,33 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(b){
+        this.sling.bodyA = b;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke(48,22,8);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop();
+
+        }
+    }
+    
+}
